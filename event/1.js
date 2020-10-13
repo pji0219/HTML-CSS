4c768a75b73bab9706fb0c48bcc7d68f55f9c8cd
park jong ik <pji219@naver.com>
2020-10-13 23:21:47 +0900

    이벤트 연습

---
+++ event/1.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('btn');
+
+btn.onclick = alert('hi');
+
+/* function Hello () {
+    alert('hi');
+}
+
+function init() {
+    hello();
+}
+
+
+btn.onclick = function() {
+    alert('hi');
+}
+
+
+btn.onclick = () => {
+    alert('hi');
+} */
---
+++ event/2.js
@@ -0,0 +1,7 @@
+const lgt = document.querySelector('body');
+const btn1 = document.querySelector('input');
+
+btn1.onclick = () => {
+    lgt.style.backgroundColor = 'black';
+    lgt.style.color = 'white';
+}
